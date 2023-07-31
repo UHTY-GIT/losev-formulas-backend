d1450e49e2f0342b4dfe675ddf7e1e25247d1963
Kolya0909 <bond410@ukr.net>
2023-07-31 11:57:17 +0300

    update_redis_conf

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-redis_config =  Rails.env.development? ? { host: '127.0.0.1', port: 6379 } : { host: 'redis://dev-api.losev-formulas.com', port: 6379}
+redis_config =  Rails.env.development? ? { host: '127.0.0.1', port: 6379 } : { host: 'http://dev-api.losev-formulas.com', port: 6379}
 
 
 $redis = Redis::Namespace.new("LOSEV_FORMULAS", redis: Redis.new(redis_config) )
